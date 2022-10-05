d73a8536d3a9daf0941b70b80994254a17c06926
MATHILDE <mathildelecontelagaillarde@gmail.com>
2022-10-05 22:18:02 +0200

    Premier git

---
+++ config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php 
+
+//Chargement de l'autoloader de composer
+require_once __DIR__."/../vendor/autoload.php";
+
+
+//chargement des variables d'environnement
+//nous allons chercher sur internet pour chercher une library https://github.com/vlucas/phpdotenv
---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * 
+ * Bienvenue dans notre framework fait maison
+ * 
+ * L'index.php représente le "FrontController"
+ * Ses rôles :
+ * -charger le fichier de configuration
+ * -creer une nouvelle instance du noyau
+ * -récuper la réponse qui sera ensuie envoyée au navigateur
+ *
+ */
+
+ //chargement du fichier de configuration
+//Charger les varibles d'environnement
+
+//Création d'une nouvelle instance du noyau de l'application
+
+//Soumission de la requête au noyau
+//Récupération de la réponse
+
+//Envoi de la réponse au navigateur
